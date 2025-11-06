b383ccd8e4c730a7b43bcae1fe6944e264e70023
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2025-11-06 16:33:31 -0700

    Old server mod cleanup

--- EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
+++ EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0+c68735ac62e3f89227994152ac7f1601dfda5dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0+9e449ded8e9fcd1a9f4e11520fb642839eb79c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("4.0.0.0")]
