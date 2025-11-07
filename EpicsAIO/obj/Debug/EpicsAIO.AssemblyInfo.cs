033648d3a1295cc09b05eaa7290dce1af41e78d8
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2025-11-07 01:20:51 -0700

    Final Commit for 4.0 dev

--- EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
+++ EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0+b383ccd8e4c730a7b43bcae1fe6944e264e70023")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.0+2813ac74929caeadfbf8658c4e37219cffd84451")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("4.0.0.0")]
