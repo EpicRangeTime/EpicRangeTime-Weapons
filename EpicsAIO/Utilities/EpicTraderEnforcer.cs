40ccb4c9fc7686f95080571ac7d0c55dbd9ae29a
bushtail <evannosich@icloud.com>
2025-11-09 20:31:33 -0500

    Ensure Badger is unlocked if the player was equal to or greater than level 12 prior to installing the mod.

---
+++ EpicsAIO/Utilities/EpicTraderEnforcer.cs
@@ -0,0 +1,23 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+
+namespace EpicsAIO.Utilities;
+
+[Injectable(TypePriority = OnLoadOrder.PostSptModLoader + 1)]
+public class EpicTraderEnforcer(ProfileHelper profileHelper) : IOnLoad
+{
+    public Task OnLoad()
+    {
+        var profiles = profileHelper.GetProfiles();
+        foreach (var profile in profiles.Select(profilePair => profilePair.Value))
+        {
+            if (profile.CharacterData?.PmcData?.TradersInfo["bd3a8b28356d9c6509966546"] == null) return Task.CompletedTask;
+            if (profile.CharacterData?.PmcData?.Info?.Level >= 12)
+            {
+                profile.CharacterData.PmcData.TradersInfo["bd3a8b28356d9c6509966546"].Unlocked = true;
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
