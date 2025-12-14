1759fbbca80bb3384ab7390b576e89acbac73800
bushtail <evannosich@icloud.com>
2025-12-13 23:33:25 -0500

    Universal trader carryhandle editor

---
+++ EpicsAIO/Utilities/TraderEdits.cs
@@ -0,0 +1,19 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Servers;
+
+namespace EpicsAIO.Utilities;
+
+[Injectable(TypePriority = OnLoadOrder.TraderRegistration - 1)]
+public class TraderEdits(DatabaseServer databaseServer) : IOnLoad
+{
+    public Task OnLoad()
+    {
+        var traders = databaseServer.GetTables().Traders;
+        foreach (var item in traders.Values.Select(trader => trader.Assort).Select(assort => assort.Items).SelectMany(items => items.Where(item => item.Template == ItemTpl.IRONSIGHT_AR15_REAR_SIGHT_CARRY_HANDLE && item.SlotId == "mod_sight_rear")))
+        {
+            item.SlotId = "mod_scope";
+        }
+        return Task.CompletedTask;
+    }
+}
