5002b1091452e17a818f1b8ff09ac9a97e2150f2
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2026-01-02 00:04:18 -0700

    Laser fixes and Handguard remasters

--- EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
+++ EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.1+30f62e660b6f1e3f33a5a55df28366f268d2ae61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.1+a8fc4bd1d1a917cca2d82682051f7f071a2ebf9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("4.0.1.0")]
