29572f23e39dbfa81791ccb77cc5c03abd33e072
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2026-06-19 14:53:59 -0600

    Fix Fiber Optic TA31

--- EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
+++ EpicsAIO/obj/Debug/EpicsAIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("4.0.8.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.8+f24a89a60a0371f9047304283bfb184968bceff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.0.8+9323268e0d0975c6812ab2f56f93e5ac5ca9c782")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpicsAIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("4.0.8.0")]
